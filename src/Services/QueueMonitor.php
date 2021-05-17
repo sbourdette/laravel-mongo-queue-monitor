517cdc3ca459de5e6ad0318fb6fdb8a299f185a5
Sylvain <sylvain@bourdette.com>
2021-05-17 11:06:31 +0200

    add Throwable to manage Null Monitor

--- src/Services/QueueMonitor.php
+++ src/Services/QueueMonitor.php
@@ -18,166 +18,165 @@ use Log;
 
 class QueueMonitor
 {
-    private const TIMESTAMP_EXACT_FORMAT = 'Y-m-d H:i:s.u';
+		private const TIMESTAMP_EXACT_FORMAT = 'Y-m-d H:i:s.u';
 
-    public const MAX_BYTES_TEXT = 65535;
+		public const MAX_BYTES_TEXT = 65535;
 
-    public const MAX_BYTES_LONGTEXT = 4294967295;
+		public const MAX_BYTES_LONGTEXT = 4294967295;
 
-    public static $loadMigrations = false;
+		public static $loadMigrations = false;
 
-    public static $model;
+		public static $model;
 
-    /**
-     * Get the model used to store the monitoring data.
-     *
-     * @return \sbourdette\MongoQueueMonitor\Models\Contracts\MonitorContract
-     */
-    public static function getModel(): MonitorContract
-    {
-        return new self::$model();
-    }
+		/**
+		 * Get the model used to store the monitoring data.
+		 *
+		 * @return \sbourdette\MongoQueueMonitor\Models\Contracts\MonitorContract
+		 */
+		public static function getModel(): MonitorContract
+		{
+				return new self::$model();
+		}
 
-    /**
-     * Handle Job Processing.
-     *
-     * @param JobProcessing $event
-     *
-     * @return void
-     */
-    public static function handleJobProcessing(JobProcessing $event): void
-    {
+		/**
+		 * Handle Job Processing.
+		 *
+		 * @param JobProcessing $event
+		 *
+		 * @return void
+		 */
+		public static function handleJobProcessing(JobProcessing $event): void
+		{
 			self::jobStarted($event->job);
-    }
+		}
 
-    /**
-     * Handle Job Processed.
-     *
-     * @param JobProcessed $event
-     *
-     * @return void
-     */
-    public static function handleJobProcessed(JobProcessed $event): void
-    {
-        self::jobFinished($event->job);
-    }
+		/**
+		 * Handle Job Processed.
+		 *
+		 * @param JobProcessed $event
+		 *
+		 * @return void
+		 */
+		public static function handleJobProcessed(JobProcessed $event): void
+		{
+				self::jobFinished($event->job);
+		}
 
-    /**
-     * Handle Job Failing.
-     *
-     * @param JobFailed $event
-     *
-     * @return void
-     */
-    public static function handleJobFailed(JobFailed $event): void
-    {
-      self::jobFinished($event->job, true, $event->exception);
-    }
+		/**
+		 * Handle Job Failing.
+		 *
+		 * @param JobFailed $event
+		 *
+		 * @return void
+		 */
+		public static function handleJobFailed(JobFailed $event): void
+		{
+			self::jobFinished($event->job, true, $event->exception);
+		}
 
-    /**
-     * Handle Job Exception Occurred.
-     *
-     * @param JobExceptionOccurred $event
-     *
-     * @return void
-     */
-    public static function handleJobExceptionOccurred(JobExceptionOccurred $event): void
-    {
-      self::jobFinished($event->job, true, $event->exception);
-    }
+		/**
+		 * Handle Job Exception Occurred.
+		 *
+		 * @param JobExceptionOccurred $event
+		 *
+		 * @return void
+		 */
+		public static function handleJobExceptionOccurred(JobExceptionOccurred $event): void
+		{
+			self::jobFinished($event->job, true, $event->exception);
+		}
 
 
 		/**
-     * Handle Job Queued.
-     *
-     * @param JobQueued $event
-     *
-     * @return void
-     */
-    public static function handleJobQueued(JobQueued $event): void
-    {
+		 * Handle Job Queued.
+		 *
+		 * @param JobQueued $event
+		 *
+		 * @return void
+		 */
+		public static function handleJobQueued(JobQueued $event): void
+		{
 				self::jobQueued($event);
-    }
-
-    /**
-     * Get Job ID.
-     *
-     * @param JobContract $job
-     *
-     * @return string|int
-     */
-    public static function getJobId(JobContract $job)
-    {
-        if ($jobId = $job->getJobId()) {
-            return $jobId;
-        }
-
-        return sha1($job->getRawBody());
-    }
+		}
 
 		/**
-     * Start Queue Monitoring for Job.
-     *
-     * @param JobContract $job
-     *
-     * @return void
-     */
-    protected static function jobQueued(JobQueued $event): void
-    {
+		 * Get Job ID.
+		 *
+		 * @param JobContract $job
+		 *
+		 * @return string|int
+		 */
+		public static function getJobId(JobContract $job)
+		{
+				if ($jobId = $job->getJobId()) {
+						return $jobId;
+				}
+
+				return sha1($job->getRawBody());
+		}
+
+		/**
+		 * Start Queue Monitoring for Job.
+		 *
+		 * @param JobContract $job
+		 *
+		 * @return void
+		 */
+		protected static function jobQueued(JobQueued $event): void
+		{
 				// $event->job is an App\Jobs\XXXX instance and not an Illuminate\Contracts\Queue\Job
-        if ( ! self::shouldBeMonitoredClassName(get_class($event->job))) {
-            return;
-        }
+				if ( ! self::shouldBeMonitoredClassName(get_class($event->job))) {
+						return;
+				}
 
-        $now = Carbon::now();
+				$now = Carbon::now();
 
-        $model = self::getModel();
+				$model = self::getModel();
 
 				try {
 
-	        $model::create([
-	            'job_id' => $event->id,
-	            'name' => get_class($event->job),
-	            'queue' => $event->job->queue ?? 'default',
-							'queued_at' => $now,
-	            'queued_at_exact' => $now->format(self::TIMESTAMP_EXACT_FORMAT),
-	            'started_at' => Null,
-	            'started_at_exact' => Null,
-	            'attempt' => 0,
-							'finished_at' => Null,
-							'finished_at_exact' => Null,
+					$model::create([
+						'job_id' => $event->id,
+						'name' => get_class($event->job),
+						'queue' => $event->job->queue ?? 'default',
+						'queued_at' => $now,
+						'queued_at_exact' => $now->format(self::TIMESTAMP_EXACT_FORMAT),
+						'started_at' => Null,
+						'started_at_exact' => Null,
+						'attempt' => 0,
+						'finished_at' => Null,
+						'finished_at_exact' => Null,
 
-	        ]);
+					]);
 				}
 				catch(Throwable $e) {
-							Log::error('sbourdette/MongoQueueMonitor - jobStarted - unable to create Monitor : ' . $e);
+							Log::error('sbourdette/MongoQueueMonitor - jobQueued - unable to create Monitor');
+							Log::debug($e);
 					}
 
-    }
+		}
 
 
-    /**
-     * Start Queue Monitoring for Job.
-     *
-     * @param JobContract $job
-     *
-     * @return void
-     */
-    protected static function jobStarted(JobContract $job): void
-    {
-        if ( ! self::shouldBeMonitored($job)) {
-            return;
-        }
+		/**
+		 * Start Queue Monitoring for Job.
+		 *
+		 * @param JobContract $job
+		 *
+		 * @return void
+		 */
+		protected static function jobStarted(JobContract $job): void
+		{
+				if ( ! self::shouldBeMonitored($job)) {
+						return;
+				}
 
-        $now = Carbon::now();
+				$now = Carbon::now();
 
-	        $model = self::getModel();
-
-//					$model::where('job_id', self::getJobId($job))->delete();
+					$model = self::getModel();
 
 					$monitor = $model::query()
-	            ->where('job_id', self::getJobId($job))
-	            ->first();
+							->where('job_id', self::getJobId($job))
+							->first();
 
 					$attributes = [
 						'name' => $job->resolveName(),
@@ -194,61 +193,61 @@ class QueueMonitor
 						Log::error('sbourdette/MongoQueueMonitor - jobStarted - unable to update Monitor : Monitor job id = ' . self::getJobId($job) . ' exists ? ');
 						Log::debug($e);
 				}
-    }
+		}
 
-    /**
-     * Finish Queue Monitoring for Job.
-     *
-     * @param JobContract $job
-     * @param bool $failed
-     * @param Throwable|null $exception
-     *
-     * @return void
-     */
-    protected static function jobFinished(JobContract $job, bool $failed = false, ?Throwable $exception = null): void
-    {
-        if ( ! self::shouldBeMonitored($job)) {
-            return;
-        }
+		/**
+		 * Finish Queue Monitoring for Job.
+		 *
+		 * @param JobContract $job
+		 * @param bool $failed
+		 * @param Throwable|null $exception
+		 *
+		 * @return void
+		 */
+		protected static function jobFinished(JobContract $job, bool $failed = false, ?Throwable $exception = null): void
+		{
+				if ( ! self::shouldBeMonitored($job)) {
+						return;
+				}
 
-        $model = self::getModel();
+				$model = self::getModel();
 
-        $monitor = $model::query()
-            ->where('job_id', self::getJobId($job))
-            ->where('attempt', $job->attempts())
-            ->orderByDesc('started_at')
-            ->first();
+				$monitor = $model::query()
+						->where('job_id', self::getJobId($job))
+						->where('attempt', $job->attempts())
+						->orderByDesc('started_at')
+						->first();
 
-        /** @var MonitorContract $monitor */
-        $now = Carbon::now();
+				/** @var MonitorContract $monitor */
+				$now = Carbon::now();
 
 				try {
-	        if ($startedAt = $monitor->getStartedAtExact()) {
-	            $timeElapsed = (float) $startedAt->diffInSeconds($now) + $startedAt->diff($now)->f;
-	        }
+					if ($startedAt = $monitor->getStartedAtExact()) {
+							$timeElapsed = (float) $startedAt->diffInSeconds($now) + $startedAt->diff($now)->f;
+					}
 
-	        /** @var IsMonitored $resolvedJob */
-	        $resolvedJob = $job->resolveName();
+					/** @var IsMonitored $resolvedJob */
+					$resolvedJob = $job->resolveName();
 
-	        if (null === $exception && false === $resolvedJob::keepMonitorOnSuccess()) {
-	            $monitor->delete();
-	            return;
-	        }
+					if (null === $exception && false === $resolvedJob::keepMonitorOnSuccess()) {
+							$monitor->delete();
+							return;
+					}
 
-	        $attributes = [
-	            'finished_at' => $now,
-	            'finished_at_exact' => $now->format(self::TIMESTAMP_EXACT_FORMAT),
-	            'time_elapsed' => $timeElapsed ?? 0.0,
-	            'failed' => $failed,
-	        ];
+					$attributes = [
+							'finished_at' => $now,
+							'finished_at_exact' => $now->format(self::TIMESTAMP_EXACT_FORMAT),
+							'time_elapsed' => $timeElapsed ?? 0.0,
+							'failed' => $failed,
+					];
 
-	        if (null !== $exception) {
-	            $attributes += [
-	                'exception' => mb_strcut((string) $exception, 0, min(PHP_INT_MAX, self::MAX_BYTES_LONGTEXT)),
-	                'exception_class' => get_class($exception),
-	                'exception_message' => mb_strcut($exception->getMessage(), 0, self::MAX_BYTES_TEXT),
-	            ];
-	        }
+					if (null !== $exception) {
+							$attributes += [
+									'exception' => mb_strcut((string) $exception, 0, min(PHP_INT_MAX, self::MAX_BYTES_LONGTEXT)),
+									'exception_class' => get_class($exception),
+									'exception_message' => mb_strcut($exception->getMessage(), 0, self::MAX_BYTES_TEXT),
+							];
+					}
 
 					$monitor->update($attributes);
 				}
@@ -256,32 +255,32 @@ class QueueMonitor
 					Log::error('sbourdette/MongoQueueMonitor - jobFinished - unable to update Monitor : Monitor job id = ' . self::getJobId($job) . ' exists ? ');
 					Log::debug($e);
 				}
-    }
-
-    /**
-     * Determine weather the Job should be monitored, default true.
-     *
-     * @param JobContract $job
-     *
-     * @return bool
-     */
-    public static function shouldBeMonitored(JobContract $job): bool
-    {
-        return array_key_exists(IsMonitored::class, ClassUses::classUsesRecursive(
-            $job->resolveName()
-        ));
-    }
+		}
 
 		/**
-     * Determine weather the JobClass should be monitored, default true.
-     *
-     * @param String $classname
-     *
-     * @return bool
-     */
-    public static function shouldBeMonitoredClassName(String $className): bool
-    {
-        return array_key_exists(IsMonitored::class, ClassUses::classUsesRecursive($className));
-    }
+		 * Determine weather the Job should be monitored, default true.
+		 *
+		 * @param JobContract $job
+		 *
+		 * @return bool
+		 */
+		public static function shouldBeMonitored(JobContract $job): bool
+		{
+				return array_key_exists(IsMonitored::class, ClassUses::classUsesRecursive(
+						$job->resolveName()
+				));
+		}
+
+		/**
+		 * Determine weather the JobClass should be monitored, default true.
+		 *
+		 * @param String $classname
+		 *
+		 * @return bool
+		 */
+		public static function shouldBeMonitoredClassName(String $className): bool
+		{
+				return array_key_exists(IsMonitored::class, ClassUses::classUsesRecursive($className));
+		}
 
 }
